ef26a01d855cdb32b9c41a107dda4412b69044ac
Atharv Jadhav <151068330+atharvv1820@users.noreply.github.com>
2024-01-25 22:48:16 +0530

    Add files via upload
    Armstrong number

---
+++ armstrong.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class armstrong
+{
+   public static void main(String args[])
+   {
+      int n,num,d,sum=0;
+	  Scanner sc=new Scanner(System.in);
+	  System.out.println("Enter a number to check armstrong");
+	  n=sc.nextInt();
+	  num=n;
+	  while(n>0)
+	  {
+	     d=num%10;
+		 sum=sum+(d*d*d);
+		 num=num/10;
+	  }
+	  if(num==sum)
+	  {
+	     System.out.println("Given number is armstrong");
+	  }
+	  else
+	  {
+         System.out.println("Given number is not armstrong");	  
+	  }
+   }
+}
